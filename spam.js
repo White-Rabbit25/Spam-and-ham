17933de9f71c8c1f08fb86539467ca76c67556d3
Студент <ab2@EU.NET>
2018-05-31 10:28:10 +0300

    rabbit

--- spam.js
+++ spam.js
@@ -2,19 +2,32 @@ let fs = require('fs');
 
 let getCategory = (line) => line.startsWith('ham') ? 'ham' : 'spam';
 
-fs.readFileSync('spam.csv')
+let lines = fs.readFileSync('spam.csv')
     .toString()
-    .split(/\r?\n/)
-    .forEach(function(line){
+    .split(/\r?\n/);
+
+let spamNumber = 0;
+let hamNumber = 0;
+
+lines.forEach((line) => {
         let category = getCategory(line);
-        console.log(category);
+        console.log(category == 'spam');
+        
+        if (category == 'spam')
+            spamNumber++;
+        else 
+           hamNumber++;
 
         line = line.substring(category.length + 1);
         let words = line.split(/[\.,"' ]+/g); 
         let lowerCaseWords = words.map((w) => w.toLowerCase());
         let cleanWords = lowerCaseWords.filter((w) => w != '');
-        console.log(cleanWords);
-        
-    });
-
+        //console.log(cleanWords);
+    }); 
+ console.log(lines.length);
+ console.log(spamNumber);
+ console.log(hamNumber);
+ 
+ 
+ 
 // See https://regexr.com/ to debug regular expressions
